a5dc1b9e95cf76fbedf6a4cb77094ad6bf8210a6
Aidan <aidanreynolds715@gmail.com>
2023-03-27 12:34:21 -0400

    fix nav bar mobile

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Navbars() {
     return (
-        <nav className='flex flex-col bg-gray-400 top-0 z-10 text-white justify-right md:sticky md:mr-auto md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center' role='navigation'>
+        <nav className='flex flex-col bg-gray-400 top-0 z-10 text-white justify-right md:sticky md:mr-auto md:border-l md:border-gray-700' role='navigation'>
             <a href='#' className='pl-8'>
                 <h1 className='text-2xl'>Aidan Reynolds</h1>
             </a>
-            <div className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-700'>
+            <div className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-700 flex flex-wrap items-center text-base justify-center'>
                 <a href='#about' className='p-4'>About</a>
                 <a href='#skills' className='p-4'>Skills</a>
                 <a href='#projects' className='p-4'>Projects</a>
