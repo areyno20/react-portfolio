b49adb1a816568617494fcf59f72eca32109ef33
Aidan <aidanreynolds715@gmail.com>
2023-03-27 12:54:45 -0400

    fix style navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Navbars() {
     return (
-        <nav className='flex flex-col bg-gray-400 top-0 z-10 text-white justify-right md:sticky md:mr-auto md:border-l md:border-gray-700' role='navigation'>
-            <h1 className='text-2xl m-5'>Aidan Reynolds</h1>
-            <div className='     md:border-gray-700 flex flex-wrap items-center text-base justify-center'>
-                <a href='#about' className='p-2'>About</a>
-                <a href='#skills' className='p-2'>Skills</a>
+        <nav className='flex flex-col bg-gray-400 top-0 z-10 text-white md:sticky md:mr-auto md:border-l md:border-gray-700' role='navigation'>
+            <div className=' md:border-gray-700 flex flex-wrap items-center text-base'>
+            <h1 className='text-2xl m-5 justify-start'>Aidan Reynolds</h1>
+                <a href='#about' className='p-2 border-l-2'>About</a>
+                <a href='#skills' className='p-2 '>Skills</a>
                 <a href='#projects' className='p-2'>Projects</a>
                 <a href='#resume' className='p-2'>Resume</a>
                 <a href='#contact' className='p-2'>Contact</a>
