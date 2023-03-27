9e4f23dd64735112139bcb78414a2c3ab2767fbe
Aidan <aidanreynolds715@gmail.com>
2023-03-27 12:49:06 -0400

    fix resume mobile

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,9 +5,11 @@ export default function Resume() {
     return (
         <section id="resume" className='my-5'>
             <div className='justify-center shadow-xl  p-4'>
-                <div>
-                    <embed src={resumeFile} width="100%" height="1000px" />
-                </div>
+            <object data={resumeFile} type="application/pdf" width="100%" height="500px" className="">
+                <p>Your web browser doesn't have a PDF plugin.
+                Instead you can <a href={resumeFile}>click here to
+                download the PDF file.</a></p>
+            </object>
             </div>
         </section>
       );
