4c8eb80d6459c309007fdac5d8735fbd904be602
Aidan <aidanreynolds715@gmail.com>
2023-03-26 23:59:06 -0400

    addition of files and edits

---
+++ src/components/contact.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default function Contact() {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [message, setMessage] = React.useState('');
+
+    function encode(data) {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+    function handleSubmit(e) {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", name, email, message })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+            
+    }
+
+    return (
+        <section className='my-5'>
+            <div className='flex flex-wrap justify-center'>
+                <div className='w-1/2 md:w-1/3 lg:w-1/2 p-10 justify-center'>
+                    <div className='bg-gray-400 p-6 justify-center'>
+                        <h3 className='text-xl text-white font-bold'>Contact Me</h3>
+                        <form name="contact" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+                            <p>
+                                <label>Your Name: <input type="text" name="name" value={name} onChange={e => setName(e.target.value)} /></label>
+                            </p>
+                            <p>
+                                <label>Your Email: <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} /></label>
+                            </p>
+                            <p>
+                                <label>Message: <textarea name="message" value={message} onChange={e => setMessage(e.target.value)} /></label>
+                            </p>
+                            <p>
+                                <button type="submit">Send</button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+
+    
---
+++ src/components/skills.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+export default function Skills() {
+    return (
+        <section className=' bg-gray-600 shadow-lg p-4 text-white'>
+            <div className=''>
+                <h1 className='text-2xl p-2 text-white font-bold bg-gray-600 w-1/6'>Skills</h1>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>HTML</h3>
+                            <p className='text-white'>I have experience in HTML5 and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>CSS</h3>
+                            <p className='text-white'>I have experience in CSS3 and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>JavaScript</h3>
+                            <p className='text-white'>I have experience in JavaScript and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>React</h3>
+                            <p className='text-white'>I have experience in React and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>Node.js</h3>
+                            <p className='text-white'>I have experience in Node.js and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>Express.js</h3>
+                            <p className='text-white'>I have experience in Express.js and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+                    <div className='w-1/6 md:w-1/3 lg:w-1/2 p-4 justify left'>
+                        <div className='bg-gray-400 p-3'>
+                            <h3 className='text-xl text-white font-bold'>MongoDB</h3>
+
+                            <p className='text-white'>I have experience in MongoDB and have used it to create websites and web applications.</p>
+                        </div>
+                    </div>
+            </div>
+        </section>
+    )
+}
